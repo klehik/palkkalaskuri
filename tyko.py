2deebc1a525608156e2fdbdfe21fb7fc86984a07
AA4085 <aa4085@student.jamk.fi>
2023-03-31 12:11:50 +0300

    subprocess script formatted

--- tyko.py
+++ tyko.py
@@ -1,52 +1,50 @@
-
 import PyPDF2
 import sys
 import os
 import json
 import re
 
-def main(argv):
 
+def main(argv):
     # get pdf and salary info from arguments
     pdf = sys.argv[1]
     wage = sys.argv[2]
     tax = sys.argv[3]
 
-    
-
     # extract text from pdf
     file = extractData(pdf)
-    
+
     # get relevant info from text
     extras = []
-    
-    
-    generateData(file, extras)
-    
-    data = {"extras": extras, "other": {"wage": wage, "tax": tax, "wasValidFile": valid}}
+    valid = True
+    generateData(file, extras, valid)
+
+    data = {
+        "extras": extras,
+        "other": {"wage": wage, "tax": tax, "wasValidFile": valid},
+    }
     # calculate
     # calculate(data)
 
     # convert to json, utf-8
-    data = json.dumps(data, ensure_ascii=False).encode('utf8')
-    
+    data = json.dumps(data, ensure_ascii=False).encode("utf8")
+
     # output data
     print(data.decode())
-    # remove pdf from server
+    # remove pdf from
     os.remove(pdf)
 
 
 def extractData(pdf):
-    
     # creating a pdf file object
-    pdfFileObj = open(pdf, 'rb')
-    
+    pdfFileObj = open(pdf, "rb")
+
     # creating a pdf reader object
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    
+
     # creating a page object
     pageObj = pdfReader.getPage(0)
-    
+
     # extracting text from page
     file = pageObj.extractText()
     pdfFileObj.close()
@@ -54,33 +52,57 @@ def extractData(pdf):
     return file
 
 
-def generateData(file, data):
+def get_hours(arr):
+    value = ""
+    i = len(arr)
+    flag = False
+    chars = []
+    for char in arr:
+        if char != " ":
+            chars.append(char)
+
+    for char in chars:
+        if char == ")":
+            flag = True
+            continue
+        if char == ":":
+            i = 2
+        if flag:
+            value += char
+        if i == 0:
+            flag = False
+            break
+        i -= 1
+    return value
+
+
+def generateData(file, data, valid):
     # remove line changes from file
     file = re.sub(r"[\n]*", "", file)
-    
+
     if ("suunnitteluraja") not in file:
-        global valid 
         valid = False
         return
-    
-    valid = True
 
-    
-    label = ["sunnuntaityö", "iltatyö", "yötyö", "lauantaityö", "50% ylityö", "100% ylityö", "hälytysraha" ]
+    label = [
+        "sunnuntaityö",
+        "iltatyö",
+        "yötyö",
+        "lauantaityö",
+        "50% ylityö",
+        "100% ylityö",
+        "hälytysraha",
+    ]
     multipliers = [1, 0.15, 0.4, 0.2, 1.5, 2, 150]
-    
-    
-   
-    
-
 
     for label, multiplier in zip(label, multipliers):
         if label not in file:
-            return
+            continue
 
-       
         if label == "iltatyö" or label == "yötyö":
-            value = file.split(label, maxsplit=1)[1].split()[1]
+            split = file.split(label, maxsplit=1)[1]
+            value = get_hours(split)
+
         elif label == "hälytysraha":
             value = file.split(label, maxsplit=1)[1].split()[1]
             item = {"label": label, "quantity": int(value), "multiplier": multiplier}
@@ -88,28 +110,27 @@ def generateData(file, data):
             continue
         else:
             value = file.split(label, maxsplit=1)[1].split()[0]
-            
-           
-         
 
         temp = value.split(":")
         hours = int(temp[0])
         minutes = int(temp[1])
 
-        totalHours = round(hours + (minutes / 60),2)
+        totalHours = round(hours + (minutes / 60), 2)
 
         item = {"label": label, "quantity": totalHours, "multiplier": multiplier}
-        
+
         data.append(item)
-        
-    
+
+
 """ def calculate(data):
     
     brutto = 0
+
     for item in data:
         label = item["lisa"]
         hours = item["tunnit"]
         multiplier = item["kerroin"]
+
         itemBrutto = round(hours * 18.07 * multiplier,2)
         
         brutto += itemBrutto
@@ -120,8 +141,7 @@ def generateData(file, data):
     
     
     totNetto = round(totBrutto - round(totBrutto * 0.19, 2) - round(totBrutto * 0.014,2) - round(totBrutto * 0.0715, 2) - round(totBrutto * 0.011,2),2) """
-    
+
 
 if __name__ == "__main__":
     main(sys.argv)
-    
